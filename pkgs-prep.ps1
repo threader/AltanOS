b4629622479c3dfdd7e1c239e2ff0f14816dca2a
threader <michael.heltne@gmail.com>
2024-08-03 02:21:34 +0200

    This is why its a good idea to review commits maybe just a bit.. I think this mostly fixes it, needs testing still

--- pkgs-prep.ps1
+++ pkgs-prep.ps1
@@ -1,8 +1,12 @@
-$DesktopPath = [Environment]#GetFolderPath("Desktop")
-$usedir = $DesktopPath\AltanOS.inst\
-if (-not (Test-Path -Path $usedir))
-	mkdir $usedir
-}
+# $DesktopPath = [Environment]::GetFolderPath("Desktop")
+string sysdrive = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%");
+string altanosdir = $sysdrive\AltanOS
+string altanosinstdir = $sysdrive\AltanOS.inst
+
+#$usedir = $altanosinstdir
+#if (-not (Test-Path -Path $usedir))
+#	mkdir $usedir
+#}
 
 Write-Output "Removing non-essential packages and installing some bare minimums"
  Get-AppPackage | Remove-AppPackage
@@ -23,11 +27,11 @@ Write-Output "Removing non-essential packages and installing some bare minimums"
  Get-AppxPackage -allusers Microsoft.MSPaint* | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
  Get-AppxPackage -allusers Microsoft.MicrosoftSolitaireCollection* | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
 
-if (-not (Test-Path "$DesktopPath\AltanOS.inst\Microsoft.DesktopAppInstaller.msixbundle")) {
+if (-not (Test-Path "$altanosinstdir\Microsoft.DesktopAppInstaller.msixbundle")) {
 	write-output "Winget not found. Grab and install" 
-	Invoke-WebRequest -uri https://github.com/microsoft/winget-cli/releases/download/v1.8.1911/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile $DesktopPath\AltanOS.inst\Microsoft.DesktopAppInstaller.msixbundle
+	Invoke-WebRequest -uri https://github.com/microsoft/winget-cli/releases/download/v1.8.1911/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile $altanosinstdir\Microsoft.DesktopAppInstaller.msixbundle
 }
-add-appxpackage -Path "$DesktopPath\AltanOS.inst\Microsoft.DesktopAppInstaller.msixbundle"
+add-appxpackage -Path "$altanosinstdir\Microsoft.DesktopAppInstaller.msixbundle"
 
 # create .xml of this eventually when it settles
 Write-Output "Install applications:"
@@ -51,6 +55,7 @@ winget install --disable-interactivity --accept-source-agreements --id Nlitesoft
 winget install --disable-interactivity --accept-source-agreements --id Malwarebytes.Malwarebytes --source winget
 winget install --disable-interactivity --accept-source-agreements --id SaferNetworking.SpybotAntiBeacon --source winget
 winget install --exact --id SomePythonThings.WingetUIStore --source winget
+
 Write-Output "Remove applications:"
 winget remove --id Microsoft.Edge --accept-source-agreements --disable-interactivity
 # winget remove --id Microsoft Edge Update* --accept-source-agreements --disable-interactivity # uuh, is this needed for Microsoft.EdgeWebView2Runtime that one probably needs?
@@ -76,7 +81,7 @@ $trigger1 = New-ScheduledTaskTrigger -AtLogon
 $task_name = "Autorun update"
 
 function shed_task() {	
-$action = New-ScheduledTaskAction -Execute "%HOMEDRIVE%\AltanOS\autorun-update.cmd"
+$action = New-ScheduledTaskAction -Execute "PowerShell" -Argument "-NoProfile -ExecutionPolicy Bypass -File '$altanosdir\autorun-update.cmd"
 $principal = "System\Administrator"
 $settings = New-ScheduledTaskSettingsSet -WakeToRun
 $task = New-ScheduledTask -Action $action -Trigger $trigger0 -Trigger $trigger1 -Settings $settings
@@ -91,13 +96,13 @@ shed_task
 #New-ScheduledTaskTrigger -AtLogon
 #)
 # The following will probably not work and include cd% in the path instead of the actual path
-#$taskAction = New-ScheduledTaskAction -Execute "PowerShell" -Argument "-NoProfile -ExecutionPolicy Bypass -File '%cd%\autorun-update.cmd' -Output 'HTML'" -WorkingDirectory 'cd%\AltanOS'
+#$taskAction = New-ScheduledTaskAction -Execute "PowerShell" -Argument "-NoProfile -ExecutionPolicy Bypass -File '$altanosdir\autorun-update.cmd' -Output 'HTML'" -WorkingDirectory '$altanosdir'
 # Register-ScheduledTask 'Update Windows and Applications' -TaskPath 'AltanOS' -Action $taskAction -Trigger $taskTrigger
 # Get-ScheduledTask -TaskPath \AltanOS\
 
 # Aaha, so thats where i put that
 # Add to startup
-# New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Update Windows and Applications" -Value "%HOMEDRIVE%\AltanOS\autorun-update.cmd"  -PropertyType "String"
+# New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Update Windows and Applications" -Value "$altanosdir\autorun-update.cmd"  -PropertyType "String"
 
 
 
