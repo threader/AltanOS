3ea18933ab5e9d8e5a552d1e23cced79bd2826fd
threader <michael.heltne@gmail.com>
2024-08-14 05:12:20 +0200

    pkgs: Fix script, start adwcleaner and such as intended

--- src/pkgs-utils.ps1
+++ src/pkgs-utils.ps1
@@ -8,7 +8,7 @@ $altanosinstdir = "$sysdrive\AltanOS.inst"
 if (-not (Test-Path -Path $altanosinstdir)) {
 	mkdir -p $altanosinstdir\bin
 }
-Set-Location $sysdrive
+Set-Location $altanosdir
 # cd $altanosdir
 
 function Get-Machine-Architecture() {
@@ -38,43 +38,42 @@ function Get-Machine-Architecture() {
     return $ENV:PROCESSOR_ARCHITECTURE
 }
 
+ $ProgressPreference = 'SilentlyContinue'
+ 
 	    if(-not ( Get-Machine-Architecture -eq "AMD64")) {
 	    Write-Output "Assume x86"
 	    	 Invoke-WebRequest -Uri "https://github.com/radareorg/radare2/releases/download/5.9.4/radare2-5.9.4-w32.zip" -OutFile "$altanosinstdir\radare2-5.9.4-w64.zip"
 			
-			 Expand-Archive -Force $altanosinstdir\adare2-5.9.4-w32.zip $altanosinstdir\bin\radare2-5.9.4-w32 
 			 Expand-Archive -Force $altanosinstdir\adare2-5.9.4-w32.zip $altanosinstdir\bin\radare2-5.9.4-w32
 		$niarchbit = ""
 		 } else  {
 		 $niarchbit = "-64"
 		
 			Invoke-WebRequest -Uri "https://malcat.fr/latest/malcat_win64_lite.zip" -OutFile "$altanosinstdir\malcat_win64_lite.zip"
-			Invoke-WebRequest -Uri "https://github.com/radareorg/radare2/releases/download/5.9.4/radare2-5.9.4-w32.zip " -OutFile "$altanosinstdir\radare2-5.9.4-w64.zip"
+			Invoke-WebRequest -Uri "https://github.com/radareorg/radare2/releases/download/5.9.4/radare2-5.9.4-w32.zip" -OutFile "$altanosinstdir\radare2-5.9.4-w64.zip"
 			
-			 Expand-Archive -Force $altanosinstdir\alcat_win64_lite.zip $altanosinstdir\bin\malcat_win64_lite
-			 Expand-Archive -Force $altanosinstdir\adare2-5.9.4-w64.zip $altanosinstdir\bin\radare2-5.9.4-w64
+			 Expand-Archive -Force "$altanosinstdir\alcat_win64_lite.zip" "$altanosinstdir\bin\malcat_win64_lite"
+			 Expand-Archive -Force "$altanosinstdir\adare2-5.9.4-w64.zip" "$altanosinstdir\bin\radare2-5.9.4-w64"
 		 }
+	# get dev tools
+ Invoke-WebRequest -uri "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.1.2_build/ghidra_11.1.2_PUBLIC_20240709.zip" -OutFile "$altanosinstdir\ghidra_11.1.2_PUBLIC_20240709.zip"
+ Expand-Archive -Force "$altanosinstdir\ghidra_11.1.2_PUBLIC_20240709.zip" "$altanosinstdir\bin\ghidra_11.1.2_PUBLIC_20240709"
 
 function get_utils() {
- $ProgressPreference = 'SilentlyContinue'
 
- Invoke-WebRequest -uri https://downloads.malwarebytes.com/file/adwcleaner -OutFile $altanosinstdir\bin\adwcleaner.exe 
- #"$altanosinstdir"\bin\adwcleaner.exe
+s Invoke-WebRequest -uri "https://downloads.malwarebytes.com/file/adwcleaner" -OutFile "$altanosinstdir\bin\adwcleaner.exe"
+ "$altanosinstdir"\bin\adwcleaner.exe
 
- Invoke-WebRequest -uri https://tinywall.pados.hu/files/TinyWall-v3-Installer.msi -OutFile $altanosinstdir\TinyWall-v3-Installer.msi 
+ Invoke-WebRequest -uri "https://tinywall.pados.hu/files/TinyWall-v3-Installer.msi" -OutFile "$altanosinstdir\TinyWall-v3-Installer.msi"
 
- Invoke-WebRequest -uri http://www.itsamples.com/downloads/network-activity-indicator-setup$niarchbit.zip -OutFile $altanosinstdir\network-indicator$niarchbit.zip 
+ Invoke-WebRequest -uri "http://www.itsamples.com/downloads/network-activity-indicator-setup$niarchbit.zip" -OutFile "$altanosinstdir\network-indicator$niarchbit.zip"
 
- Invoke-WebRequest -uri https://privazer.com/en/PrivaZer.exe -OutFile $altanosinstdir\bin\PrivaZer.exe  
+ Invoke-WebRequest -uri "https://privazer.com/en/PrivaZer.exe" -OutFile "$altanosinstdir\bin\PrivaZer.exe"
 
- # cp $altanosdir\bin\PrivaZer.ini $altanosinstdir/bin/ 
+ cp $altanosdir\bin\PrivaZer.ini $altanosinstdir\bin\
 
-	# get dev tools
- Invoke-WebRequest -uri https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.1.2_build/ghidra_11.1.2_PUBLIC_20240709.zip -OutDir $altanosinstdir\ghidra_11.1.2_PUBLIC_20240709.zip
-$ProgressPreference = 'Continue' 
-
- Expand-Archive -Force $altanosinstdir\network-indicator$niarchbit.zip $altanosinstdir\bin\network-indicator$niarchbit
- Expand-Archive -Force $altanosinstdir\ghidra_11.1.2_PUBLIC_20240709.zip $altanosinstdir\bin\ghidra_11.1.2_PUBLIC_20240709
- Expand-Archive -Force $altanosinstdir\alcat_win64_lite.zip $altanosinstdir\bin\malcat_win64_lite
+ Expand-Archive -Force "$altanosinstdir\network-indicator$niarchbit.zip" "$altanosinstdir\bin\network-indicator$niarchbit"
 }
 get_utils
+
+$ProgressPreference = 'Continue' 
