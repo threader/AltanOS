019fdc8a48afa3909fbae7c584e772d96515b7cb
Michael Heltne <threader@users.noreply.github.com>
2024-09-18 07:21:31 +0200

    Harden: notes, we cant harden against snails crawling scross TPM module, feature denied, suggest user arms robot nanobots and considers this a user error

--- src/harden.ps1
+++ src/harden.ps1
@@ -104,7 +104,7 @@ $sysdrive =  $Env:SystemDrive
 # note :
 # It is my opinion that encrypting the entire HDD is a complete waste of read/write cycles, 
 # using something like a bellow encrypted throw away disposable VM image or really something makes more sense. 
-# nb. maintainability and backup in case of emergency ? it is afer all a lot easier to ask the user 'do you happen to remember 
+# nb. maintainability and backup in case of emergency, that emergency could be some poor animal, an ant or idk, snail crawling scross your TPM module? it is afer all a lot easier to ask the user 'do you happen to remember 
 # your users/ passwo, your windows/system password or %programfiles% or Documents password than do you remember your password and 
 #'oh yeah have you maybe seen that 128 digit hex decimal or whatever hash that flew by that one time you'r computer crashed'. 
 
